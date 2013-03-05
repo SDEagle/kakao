a557d5b3b10a2fa7fd8b0946f95f377032c12916
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2013-03-05 15:56:26 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kakao::Application.config.secret_key_base = 'a330a2b08fa72bff30b1c50e6fff8c86b6e13216f7db05ea662184757ca5db1364b2fbff8a69aeab9d4d926ea7aebbc52d3472038e3acbba5b22457cae16e03a'
